467c718addee196c37ca7d96a38a0a828c628c03
Guy Bianco IV <gjbiancoiv@gmail.com>
2021-04-07 12:49:10 -0400

    ouput graphviz and cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { idFromInfo, sectionInfoFromPath } from './section-info.ts';
 import { findReferences } from './references.ts';
 
+console.log('digraph G {');
+
 for (let i = 0; i < Deno.args.length; i++) {
   const filename = Deno.args[i];
   const info = sectionInfoFromPath(filename);
@@ -8,9 +10,9 @@ for (let i = 0; i < Deno.args.length; i++) {
     const refId = idFromInfo(info);
     const fileContents = Deno.readTextFileSync(filename);
     findReferences(fileContents).forEach((ref) => {
-      console.log(`${refId} --> ${ref}`);
+      console.log(`  "${refId}" -> "${ref}";`);
     });
-  } else {
-    console.warn('WARNING: could not get section info for:', filename);
   }
 }
+
+console.log('}');
